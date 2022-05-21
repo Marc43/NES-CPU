1b5920a8293a61c6703b7488b44224132d6f3a9b
Marc43 <marcdominguezr@gmail.com>
2022-05-21 17:26:35 +0200

    Starting from the bottom

---
+++ pkg/nes_cpu_pkg.sv
@@ -0,0 +1,7 @@
+package nes_cpu;
+   
+    int MEM_ADDR_SIZE = 16;         // 16 bits to index memory
+    int BOOT_ADDR = 0;              // TODO Not really  
+    int INSTR_SIZE_IN_BYTES = 16;   // So far 
+
+endpackage : nes_cpu
---
+++ src/pc.sv
@@ -0,0 +1,30 @@
+import nes_cpu_pkg::*;
+
+module pc 
+(
+
+    input logic clk_i,
+    input logic rstn_i,
+
+    input logic taken_branch_i,
+    input logic [MEM_ADDR_SIZE-1:0] new_pc_i,
+
+    output logic [MEM_ADDR_SIZE-1:0] pc_o
+
+);
+
+    always_ff @(posedge clk_i, negedge rstn_i) begin
+        if (rstn_i == 0) begin
+            pc_o <= BOOT_ADDR;
+        end
+        else begin
+            if (taken_branch_i) begin
+                pc_o <= new_pc_i;
+            end
+            else begin
+                pc_o <= pc_o + INSTR_SIZE_IN_BYTES;              
+            end
+        end
+    end
+
+endmodule : pc
