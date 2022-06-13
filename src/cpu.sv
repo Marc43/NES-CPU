63677cf9e1e28a5fd69d3ae5cd41b6ab84263995
Marc Dominguez <marc.dominguez@codasip.com>
2022-06-13 16:25:27 +0200

    Empty files cpu and ex

---
+++ src/cpu.sv
@@ -0,0 +1,8 @@
+module cpu_t
+(
+    input logic clk_i,
+    input logic rstn_i
+
+);
+
+endmodule : cpu_t
---
+++ src/ex.sv
@@ -0,0 +1,9 @@
+module ex_t
+(
+    input logic clk_i,
+    input logic rstn_i,
+
+);
+
+
+endmodule : ex_t
